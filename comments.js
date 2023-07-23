eeeb76f3064506046c162a826f375a24e4c397d0
japi <github@ziqu.de>
2023-07-23 22:08:33 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// Create web server
+// Listen on port 3000
+// Use express
+// Use middleware body-parser
+// Use middleware cors
+// Use middleware morgan
+// Use middleware method-override
+// Use middleware express-session
+// Use middleware express-flash
+// Use middleware connect-mongo
+// Use middleware passport
+// Use middleware passport-local
+// Use middleware passport-local-mongoose
+// Use middleware express-validator
+// Use middleware express-layouts
+// Use middleware express-partials
+// Use middleware connect-assets
+// Use middleware errorhandler
+// Use middleware express-messages
+// Use middleware express-validator
+// Use middleware express-validator-error-handler
+// Use middleware express-validator-custom-error
+// Use middleware express-validator-custom-validation
+// Use middleware multer
+// Use mid
